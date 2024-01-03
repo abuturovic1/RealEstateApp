244e80ded828459c37a587f9324b3f115b8f8620
Amina <abuturovic1@etf.unsa.ba>
2024-01-03 18:55:54 +0100

    spirala 3 commit

---
+++ index.js
@@ -0,0 +1,207 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+const app = express();
+const port = 3000;
+const saltRounds = 10;
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: 'true',
+    saveUninitialized: 'true',
+  })
+);
+
+const publicPath = path.join(__dirname, 'public');
+
+// app.use(express.static(publicPath));
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public/html')));
+app.use(express.static(path.join(__dirname, '/public/css')));
+app.use(express.static(path.join(__dirname, '/public/images')));
+app.use(express.static(path.join(__dirname, '/public/scripts')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  //   const defaultPage = 'meni';
+  //   // console.log(req);
+  //   const filePath = path.join(publicPath, 'html', `${defaultPage}.html`);
+
+  res.sendFile(path.join(__dirname + '/public/html/meni.html'));
+
+  //   fs.access(filePath, fs.constants.F_OK, err => {
+  //     if (err) {
+  //       res.status(404).send('Not Found');
+  //     } else {
+  //       res.sendFile(filePath);
+  //     }
+  //   });
+});
+
+app.post('/login', (req, res) => {
+  if (!req.body || !req.body.username || !req.body.password) {
+    res.status(401).json({ greska: 'Neuspješna prijava' });
+  } else {
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+      const { username, password } = req.body;
+      let korisnici = require('./data/korisnici.json');
+      let prijava = false;
+
+      if (hash) {
+        for (let i = 0; i < korisnici.length; i++) {
+          if (korisnici[i].username === username) {
+            prijava = true;
+            bcrypt.compare(password, korisnici[i].password, (err, result) => {
+              if (err) {
+                res.status(401).json({ greska: 'Neuspješna prijava' });
+              } else {
+                if (result) {
+                  req.session.loggedIn = true;
+                  req.session.username = username;
+                  res.status(200).json({ poruka: 'Uspješna prijava' });
+                } else {
+                  res.status(401).json({ greska: 'Neuspješna prijava' });
+                }
+              }
+            });
+          }
+        }
+      }
+      if (!prijava) {
+        res.status(401).json({ greska: 'Neuspješna prijava' });
+      }
+      if (err) {
+        console.log('Error while trying to hash password');
+        res.status(401).json({ greska: 'Neuspješna prijava' });
+      }
+    });
+  }
+});
+
+app.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.username = null;
+    req.session.loggedIn = false;
+    res.status(200).json({ poruka: 'Uspješno ste se odjavili' });
+  } else {
+    res.status(401).json({ greska: 'Neautorizovan pristup' });
+  }
+});
+
+app.get('/korisnik', function (req, res) {
+  if (req.session.loggedIn) {
+    let username = req.session.username;
+
+    let korisnici = require('./data/korisnici.json');
+
+    for (let i = 0; i < korisnici.length; i++) {
+      if (korisnici[i].username === username) {
+        res.status(200).json({
+          id: korisnici[i].id,
+          ime: korisnici[i].ime,
+          prezime: korisnici[i].prezime,
+          username: korisnici[i].username,
+          password: korisnici[i].password,
+        });
+      }
+    }
+  } else {
+    res.status(401).json({ greska: 'Neautorizovan pristup' });
+  }
+});
+
+app.post('/upit', (req, res) => {
+  if (req.session.loggedIn) {
+    const { nekretnina_id, tekst_upita } = req.body;
+
+    const nekretnine = require('./data/nekretnine.json');
+
+    const nekretnina = nekretnine.find(nekretnina => nekretnina.id == nekretnina_id);
+
+    const username = 'username2';
+
+    const korisnici = require('./data/korisnici.json');
+
+    const korisnikId = korisnici.find(korisnik => korisnik.username === username).id;
+
+    if (!nekretnina) {
+      res.status(400).json({ greska: `Nekretnina sa id-em ${nekretnina_id} ne postoji` });
+    } else {
+      nekretnina.upiti.push({
+        korisnik_id: korisnikId,
+        tekst_upita,
+      });
+      res.status(200).json({ poruka: 'Upit je uspjesno dodan' });
+    }
+  } else {
+    res.status(401).json({ greska: 'Neautorizovan pristup' });
+  }
+});
+
+app.put('/korisnik', (req, res) => {
+  if (req.session.loggedIn) {
+    const { ime, prezime, username, password } = req.body;
+    const korisnici = require('./data/korisnici.json');
+
+    const userIndex = korisnici.findIndex(user => user.username === req.session.username);
+
+    if (ime) {
+      korisnici[userIndex].ime = ime;
+    }
+    if (prezime) {
+      korisnici[userIndex].prezime = prezime;
+    }
+    if (username) {
+      korisnici[userIndex].username = username;
+    }
+    if (password) {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+          res.status(500).json({ greska: 'Greška prilikom heširanja passworda' });
+        } else {
+          korisnici[userIndex].password = hash;
+        }
+      });
+    }
+
+    fs.writeFile('./data/korisnici.json', JSON.stringify(korisnici, null, 2), err => {
+      if (err) {
+        res.status(500).json({ greska: 'Greška prilikom ažuriranja podataka korisnika' });
+        return;
+      }
+      res.status(200).json({ poruka: 'Podaci su uspješno ažurirani' });
+    });
+  } else {
+    res.status(401).json({ greska: 'Neautorizovan pristup' });
+  }
+});
+
+app.get('/nekretnine', (req, res) => {
+  let nekretnine = require('./data/nekretnine.json');
+
+  res.status(200).json({ nekretnine });
+});
+
+app.get('/:page', (req, res) => {
+  const page = req.params.page;
+  const filePath = path.join(publicPath, 'html', `${page}.html`);
+
+  fs.access(filePath, fs.constants.F_OK, err => {
+    if (err) {
+      res.status(404).send('Not Found');
+    } else {
+      res.sendFile(filePath);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+});
